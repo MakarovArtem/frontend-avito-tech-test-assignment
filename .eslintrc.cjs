b1a0a43d528b7a719f8b33079b35c07279862ff2
Artem <artemmakarov76@yandex.ru>
2023-08-26 14:29:35 +0300

    feat: setup eslint, add some rules, fix most problems

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,49 @@
 module.exports = {
+  'settings': {
+    'react': {
+      'version': 'detect',
+    }
+  },
   root: true,
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-type-checked',
+    'plugin:@typescript-eslint/stylistic-type-checked',
     'plugin:react-hooks/recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json', './tsconfig.node.json'],
+    tsconfigRootDir: __dirname,
+   },
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'quotes': ['error', 'single'],
+    'indent': ['warn', 4],
+    'curly': 'error',
+    'block-spacing': 'warn',
+    'linebreak-style': ['error', 'unix'],
+    'no-trailing-spaces': "error",
+    "object-curly-newline": ["error", {
+        "ObjectExpression": { "multiline": true },
+        "ObjectPattern": { "multiline": true },
+        "ImportDeclaration": "never",
+        "ExportDeclaration": { "multiline": true, "minProperties": 3 }
+    }],
+    'object-curly-spacing': ["error", "always"],
+    "semi": "warn",
+    "space-before-blocks": "warn",
+    "@typescript-eslint/strict-boolean-expressions": "warn",
+    "@typescript-eslint/prefer-nullish-coalescing": "warn",
   },
 }
